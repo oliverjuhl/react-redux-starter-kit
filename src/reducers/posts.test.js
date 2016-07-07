9895ac7f08b012c6c30b510720ca1e8cb0db51b6
Tomasz Szymanek <tomasz.szymanek@gmail.com>
2016-07-07 23:14:36 +0200

    [add] - now tests are using deepFreeze library

--- src/reducers/posts.test.js
+++ src/reducers/posts.test.js
@@ -1,12 +1,18 @@
 import { addPost, removePost } from './posts';
 
 import { expect } from 'chai';
+var deepFreeze = require('deep-freeze');
 
 describe('Post reducer', function() {
   describe('#addPost()', function() {
     it('should return new state with concated value', function() {
-      expect(addPost([], { payload: 'aaa' })).to.deep.equal(['aaa']);
-      expect(addPost(['bbb'], { payload: 'aaa' })).to.deep.equal(['bbb', 'aaa']);
+      const state = deepFreeze([]);
+      expect(addPost(state, { payload: 'aaa' })).to.deep.equal(['aaa']);
+    });
+
+    it('should return new state even if this state contained some values', function () {
+      const state = deepFreeze(['bbb']);
+      expect(addPost(state, { payload: 'aaa' })).to.deep.equal(['bbb', 'aaa']);
     });
   });
 
