3d5299e6a2f581c0cf08f7d9f2025de31011fbce
Tomasz Szymanek <tomasz.szymanek@gmail.com>
2016-07-09 12:15:04 +0200

    [cs] - eslint config update

--- src/components/HomeView/index.js
+++ src/components/HomeView/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 
-/*eslint-disable */
 const HomeView = ({ handleClick }) => {
-  let input = "";
+  let input = '';
   return (
   <div className="content home-form">
     <div className="home-form-title">add post</div>
-    <textarea className="home-form-input" rows="3" type="text" ref={ node => {input = node}} placeholder="Your post"></textarea>
-    <button className="home-form-button" onClick={() => {
-      if(!input.value) {
-        return;
-      }
-      handleClick(input.value);
-      input.value = '';
-    }}>add</button>
+    <textarea className="home-form-input"
+      rows="3"
+      type="text"
+      ref={ node => {input = node;}}
+      placeholder="Your post">
+    </textarea>
+    <button className="home-form-button"
+      onClick={() => {
+        if (!input.value) {
+          return;
+        }
+        handleClick(input.value);
+        input.value = '';
+      }}>add\
+    </button>
   </div>);
 };
-/*eslint-disable */
 
 HomeView.propTypes = {
   handleClick: React.PropTypes.func
